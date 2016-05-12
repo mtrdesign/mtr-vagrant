4709239d1370c4770a14a6e790c0260dcc05f9fd
Krasimir Nikolov <krasimir.nikolov@mtr-design.com>
2016-05-12 20:37:16 +0300

    Change some settings and add increase_swap.sh (added mysql 5.7, php 5.6, phpmyadmin, nvm, composer)

--- Vagrantfile
+++ Vagrantfile
@@ -4,16 +4,18 @@ require 'etc'
  
 Vagrant.configure("2") do |config|
   config.vm.box = "trusty64"
-  config.vm.box_url = "https://atlas.hashicorp.com/ubuntu/boxes/trusty64/versions/20160222.0.0/providers/virtualbox.box"
-  config.vm.hostname = "global-vagrant"
+  config.vm.box_url = "https://atlas.hashicorp.com/ubuntu/boxes/trusty64/versions/20160406.0.0/providers/virtualbox.box"
+  config.vm.hostname = "gloval-vagrant"
+  config.vm.provision "shell", path: "increase_swap.sh"
 
   # config.vm.provision :shell, :path => "bootstrap.sh", :args => Etc.getlogin
-  config.vm.synced_folder ".", "/srv/projects", :nfs => ENV.fetch('MTR_VAGRANT_USE_NFS', false)
+  config.vm.synced_folder ".", "/srv/projects", id: "vagrant-root",
+    owner: "vagrant",
+    group: "www-data",
+    mount_options: ["dmode=775", "fmode=764"], :nfs => ENV.fetch('MTR_VAGRANT_USE_NFS', false)
 
   # config.vm.network(:private_network, ip: "10.254.254.254") if ENV.fetch('MTR_VAGRANT_USE_NFS', false)
-    config.vm.network :forwarded_port, guest: 80, host: 8000       # used by Apache dev server
-  # config.vm.network :forwarded_port, guest: 9000, host: 9000       # used by Xdebug
-  #config.vm.network :forwarded_port, guest: 3306, host: 3308       # used by MySQL dev server
+  config.vm.network :forwarded_port, guest: 80, host: 8000       # used by Apache dev server
 
   config.vm.provider :virtualbox do | vb |
     vb.name = "global-vagrant"
@@ -22,4 +24,3 @@ Vagrant.configure("2") do |config|
     vb.customize ["modifyvm", :id, "--cpus", ENV.fetch('MTR_VAGRANT_CPUS', 1)]
   end
 end
-
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+NVM_VERSION=4.4.3
+
 # Color helpers
 export TERM=${TERM:-vt100} # avoid tput complaining when TERM is not set.
 RED=$(tput setaf 1)
@@ -8,6 +10,42 @@ BLUE=$(tput setaf 4)
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 
+MYSQL_LIST=$(cat <<EOF
+### THIS FILE IS AUTOMATICALLY CONFIGURED ###
+# You may comment out entries below, but any other modifications may be lost.
+# Use command 'dpkg-reconfigure mysql-apt-config' as root for modifications.
+deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-apt-config
+deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7
+deb-src http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7
+EOF
+)
+
+XDEBUG=$(cat <<EOF
+zend_extension=/usr/lib/php5/20131226/xdebug.so
+EOF
+)
+
+PHPMYADMIN_NAME=phpmyadmin
+VHOST_PHPMYADIN=$(cat <<EOF
+  <VirtualHost *:80>
+        ServerAdmin webmaster@$PHPMYADMIN_NAME.mtr
+        DocumentRoot /var/www/$PHPMYADMIN_NAME
+        <Directory /var/www/$PHPMYADMIN_NAME/>
+            Options +FollowSymLinks
+            AllowOverride All
+        </Directory>
+        ServerName www.$PHPMYADMIN_NAME.mtr
+        ErrorLog /var/log/apache2/logs/$PHPMYADMIN_NAME.www-error_log.log
+        CustomLog /var/log/apache2/logs/$PHPMYADMIN_NAME.www-access_log.log common
+  </VirtualHost>
+EOF
+)
+
+PHPMYADMINCFG=$(cat <<'EOF'
+$cfg["blowfish_secret"] = "qtdRoGmbc9{8IZr323xYcSN]0s)r$9b_JUnb{~Xz"; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+EOF
+)
+
 all() { # Configure everything on a new machine.
     install_server_libs && \
     install_apache && \
@@ -15,7 +53,9 @@ all() { # Configure everything on a new machine.
     install_php && \
     install_mysql && \
  	install_xdebug && \
-    install_memcached
+    install_memcached && \
+    set_xdebug && \
+    install_dev_tools
 }
 
 set_apache_rewrite_mode() {
@@ -46,27 +86,70 @@ install_server_libs() { # Install server libs
 	sudo apt-get install -y curl
 	sudo apt-get install -y libsslcommon2-dev
 	sudo apt-get install -y libcurl4-openssl-dev
-	sudo apt-get install -y pkg-config
     sudo apt-get install -y python-software-properties
+    sudo apt-get install -y debconf-utils
+    sudo apt-get install -y htop
     sudo apt-get update
 }
 
 install_xdebug() {
 	sudo apt-get install -y php5-dev php-pear
+    sudo apt-get install -y php5-curl
     sudo pecl install xdebug
     sudo service apache2 restart
-	# Add in /etc/php5/apache2/php.ini - zend_extension=/usr/lib/php5/20121212/xdebug.so (after restart server)
+	# Add in /etc/php5/apache2/php.ini - zend_extension=/usr/lib/php5/20131226/xdebug.so (after restart server)
+}
+
+set_xdebug() { # Install Vhost
+    sudo mkdir /var/log/apache2/logs
+    sudo sh -c "echo '${XDEBUG}' >> /etc/php5/apache2/php.ini"
+    sudo service apache2 restart
+}
+
+install_dev_tools() { # Install dev tools
+    # Add PHPMyadmin
+    cd /var/www/ && sudo wget https://files.phpmyadmin.net/phpMyAdmin/4.6.0/phpMyAdmin-4.6.0-english.tar.gz
+    cd /var/www/ && sudo tar -zxvf phpMyAdmin-4.6.0-english.tar.gz
+    cd /var/www/ && sudo mv phpMyAdmin-4.6.0-english phpmyadmin
+    cd /var/www/ && sudo rm phpMyAdmin-4.6.0-english.tar.gz
+
+    sudo touch /etc/apache2/sites-available/$PHPMYADMIN_NAME.conf
+    sudo sh -c "echo '${VHOST_PHPMYADIN}' > /etc/apache2/sites-available/$PHPMYADMIN_NAME.conf"
+    sudo a2ensite $PHPMYADMIN_NAME.conf
+    sudo service apache2 restart
+
+    sudo cp /var/www/phpmyadmin/config.sample.inc.php /var/www/phpmyadmin/config.inc.php
+    sudo sh -c "echo '${PHPMYADMINCFG}' >> /var/www/phpmyadmin/config.inc.php"
+    sudo service apache2 restart
+    
+
+    # Add Composer
+    cd $HOME && curl -sS https://getcomposer.org/installer | php
+    sudo mv composer.phar /usr/local/bin/composer
+
+    # Add NVM
+    cd $HOME && curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+    nvm install $NVM_VERSION
+    nvm use $NVM_VERSION
+    sudo sh -c "echo 'nvm use ${NVM_VERSION}' >> $HOME/.bashrc"
+
+    # Add Bower
+    npm install bower -g
 }
 
 install_mysql() { # Install mysql
-    cd $HOME && sudo wget https://dev.mysql.com/get/mysql-apt-config_0.6.0-1_all.deb
-    cd $HOME && sudo dpkg -i mysql-apt-config_0.6.0-1_all.deb
+    export DEBIAN_FRONTEND=noninteractive
+    sudo sh -c "echo '${MYSQL_LIST}' > /etc/apt/sources.list.d/mysql.list"
     sudo apt-get update
-	sudo apt-get install -y mysql-server
+    echo mysql-community-server mysql-community-server/re-root-pass password "parola" | sudo debconf-set-selections
+    echo mysql-community-server mysql-community-server/root-pass password "parola" | sudo debconf-set-selections
+	sudo -E apt-get --force-yes -y install mysql-community-server
 }
 
-install_memcached() {
-    sudo apt-get install -y php5-mysql 
+install_memcached() { 
+    sudo apt-get install -y php5-mysqlnd
     sudo apt-get install -y php5
     sudo apt-get install -y php5-memcache
     sudo apt-get install -y memcached
@@ -89,4 +172,3 @@ if [ -z "$cmd" ] ; then
 fi
 shift
 $cmd "$@"
-
